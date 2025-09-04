d6be3777d8832264386c4706f6746a8bde67f288
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2025-09-04 17:46:54 +0000

    more java files

--- patterns/NonSingleton.java
+++ patterns/NonSingleton.java
@@ -2,6 +2,9 @@
 
 public class NonSingleton {
 	
-	public NonSingleton() { }
+	public NonSingleton() { value++; staticValue++; }
 	private int value = 0;
+    private static int staticValue = 0;
+
+   public void increment() { value++; }
 }
