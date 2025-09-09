b1194ccc849a4f4145261c11520b902798b64609
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2025-09-09 12:50:20 -0400

    Update Singleton.java

--- patterns/Singleton.java
+++ patterns/Singleton.java
@@ -2,10 +2,9 @@
 
 public class Singleton {
 	
-	private Singleton() { value++; }
+	private Singleton() { }
 	
 	private static Singleton CLASS_INSTANCE = new Singleton();
-	protected int value = 0;
 	public static Singleton getInstance() {
 		return CLASS_INSTANCE;
 	}
